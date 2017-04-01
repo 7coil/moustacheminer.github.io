6713b9b1f7f9032ab0b82becde80d34cd9fe5378
7coil <leondrolio@gmail.com>
2017-04-01 22:28:16 +0100

    Replace old index.php

--- place/index.php
+++ place/index.php
@@ -1,15 +1,76 @@
 <html>
 	<head>
-		<title>Reddit Place</title>
-		<link rel="stylesheet" href="place.css">
-		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+		<link type="text/css" rel="stylesheet" href="/css/materialize.css" media="screen,projection"/>
+		<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+		<title>Reddit Place - moustacheminer.com</title>
 		<meta name="description" content="A web interface that allows you to recall your or other's past placed pixels.">
+		<style>
+			.parallax-container {
+				height: 100%;
+				width: 100%;
+			}
+
+			.background {
+				opacity: 0.3;
+			}
+
+			h1 {
+				font-size: 6vw;
+				margin-left: auto;
+				margin-right: auto;
+			}
+
+			body {
+				background-color: #FFFFFF;
+			}
+
+			footer {
+				width: 100%
+			}
+		</style>
 	</head>
 	<body>
-		<h1>Reddit Place User Search</h1>
-		<p>See what colours people have submitted (since Fri Mar 31 2017 20:26:06 GMT+0100)</p>
-		<a href="export.csv">Download a CSV of the logs stored on this website.</a>
-		<p>Send GET requests to http://moustacheminer.com/place/searchapi?username=(username here)</p>
+		<nav class="light-blue lighten-1" >
+			<div class="nav-wrapper container">
+				<div class="brand-logo">moustacheminer</div>
+				<a href="#" data-activates="side-nav" class="button-collapse"><i class="material-icons">menu</i></a>
+				<ul class="side-nav" id="side-nav">
+					<li><a href="https://www.gnu.org/licenses/gpl-3.0.en.html">GNU GPL v3</a></li>
+					<li class="no-padding">
+						<ul class="collapsible collapsible-accordion">
+							<li>
+								<a class="collapsible-header">MSS-Discord<i class="material-icons">arrow_drop_down</i></a>
+								<div class="collapsible-body">
+									<ul>
+										<li><a href="https://discordapp.com/oauth2/authorize?&client_id=257547382277931009&scope=bot&permissions=70765632">Invite Bot</a></li>
+										<li><a href="https://discord.gg/hPw5gEt">MSS Chat</a></li>
+									</ul>
+								</div>
+							</li>
+						</ul>
+					</li>
+					<li><a href="/place/">/r/place User Logs</a></li>
+					<li><a href="/place/export.csv">/r/place Exports (CSV)</a></li>
+				</ul>
+			</div>
+			<div class="nav-wrapper container">
+				<ul class="side-nav" id="github">
+					<li><a href="https://github.com/moustacheminer" class="subheader">moustacheminer @ GitHub</a></li>
+					<li><a href="https://github.com/moustacheminer/moustacheminer.github.io">moustacheminer.github.io</a></li>
+					<li><a href="https://github.com/moustacheminer/mss-discord">MSS-Discord</a></li>
+					<li><a href="https://github.com/moustacheminer/mss-garrysmod">MSS-GarrysMod</a></li>
+					<li><a href="https://github.com/moustacheminer/place">/r/place Capture</a></li>
+					<li><div class="divider"></div></li>
+					<li><a href="https://github.com/lepon01" class="subheader">7coil @ GitHub</a></li>
+					<li><a href="https://github.com/lepon01/pbblbkmchat">Pebble Watch Chat</a></li>
+				</ul>
+			</div>
+		</nav>
+		<main>
+		<div class="container">
+			<br><br>
+			<h1 class="center teal-text text-lighten-2">Reddit Place User Search</h1>
 		<form method="get">
 			Username: <input type="text" name="username">
 		</form>
@@ -118,7 +179,33 @@
 				$conn->close();
 			}
 		?>
-		<hr>
-		<i>moustacheminer.com - Copyright 2002, All rights reserved</i>
+			<br><br>
+		</div>
+		</main>
+		<script type="text/javascript" src="https://code.jquery.com/jquery-2.1.4.min.js"></script>
+		<script type="text/javascript" src="/js/materialize.min.js"></script>
+		<script type="text/javascript" src="/js/init.js"></script>
+		<footer class="page-footer light-blue lighten-1">
+			<div class="container">
+				<div class="row">
+					<div class="col s12">
+						<p class="grey-text text-lighten-4">Serving servers so sadly, some servers serving server services serve sick server serving servers.</p>
+					</div>
+				</div>
+			</div>
+			<div class="footer-copyright light-blue">
+				<div class="container">
+					&#169 2015 - 2017 moustacheminer.com
+				</div>
+			</div>
+		</footer>
+		<script>
+			$(".btn-large").sideNav();
+			$(".github").sideNav();
+
+			$('.collapsible').collapsible({
+				accordion : true
+			});
+		</script>
 	</body>
 </html>
