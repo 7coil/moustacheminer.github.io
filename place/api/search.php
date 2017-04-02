3385f90c17eb844c05e32917f60af69263a3f684
7coil <leondrolio@gmail.com>
2017-04-02 01:08:53 +0100

    force a query

--- place/api/search.php
+++ place/api/search.php
@@ -1,60 +1,63 @@
 <?php
-	ini_set('memory_limit', '1024M');
-	$servername = "127.0.0.1";
-	$serveruser = "reddit";
-	$serverpass = "redditsips";
-	$db = "reddit";
-	$rows = array();
+	if(isset($_GET['username']) || isset($_GET['x']) || isset($_GET['y']) || isset($_GET['colour']) ) {
+		$servername = "127.0.0.1";
+		$serveruser = "reddit";
+		$serverpass = "redditsips";
+		$db = "reddit";
+		$rows = array();
 
-	$conn = new mysqli($servername, $serveruser, $serverpass, $db);
+		$conn = new mysqli($servername, $serveruser, $serverpass, $db);
 
-	if($conn->connect_errno > 0){
-		die('{"error": "There was an mySQL connection error"}');
+		if($conn->connect_errno > 0){
+			die('{"error": "There was an mySQL connection error"}');
+		}
+
+		$username = mysqli_real_escape_string($conn, $_GET['username']);
+		$x = mysqli_real_escape_string($conn, $_GET['x']);
+		$y = mysqli_real_escape_string($conn, $_GET['y']);
+		$colour = mysqli_real_escape_string($conn, $_GET['colour']);
+
+		$wheres = array();
+
+		if($username) {
+			$wheres[] = 'username = "' . $username . '"';
+		}
+
+		if($x) {
+			$wheres[] = 'x = ' . $x;
+		}
+
+		if($y) {
+			$wheres[] = 'y = ' . $y;
+		}
+
+		if($colour) {
+			$wheres[] = 'colour = ' . $colour;
+		}
+
+		$where_string = implode(' AND ', $wheres);
+
+		$sql = "SELECT * FROM place";
+		if ($where_string) {
+			$sql .= " WHERE " . $where_string;
+		}
+
+		$result = $conn->query($sql) or die('{"error": "The mySQL query returned an error."}');
+
+		$rows['error'] = "false";
+
+		if(mysqli_num_rows($result) < 1) {
+			die('{"error": "No results."}');
+		}
+
+		while($row = $result->fetch_assoc()){
+			$rows['info'][] = $row;
+		}
+
+		$conn->close();
+
+		print json_encode($rows);
+	} else {
+		die('{"error": "You did not send a GET request with username, x, y and/or colour"}');
 	}
-
-	$username = mysqli_real_escape_string($conn, $_GET['username']);
-	$x = mysqli_real_escape_string($conn, $_GET['x']);
-	$y = mysqli_real_escape_string($conn, $_GET['y']);
-	$colour = mysqli_real_escape_string($conn, $_GET['colour']);
-
-	$wheres = array();
-
-	if($username) {
-		$wheres[] = 'username = "' . $username . '"';
-	}
-
-	if($x) {
-		$wheres[] = 'x = ' . $x;
-	}
-
-	if($y) {
-		$wheres[] = 'y = ' . $y;
-	}
-
-	if($colour) {
-		$wheres[] = 'colour = ' . $colour;
-	}
-
-	$where_string = implode(' AND ', $wheres);
-
-	$sql = "SELECT * FROM place";
-	if ($where_string) {
-		$sql .= " WHERE " . $where_string;
-	}
-
-	$result = $conn->query($sql) or die('{"error": "The mySQL query returned an error."}');
-
-	$rows['error'] = "false";
-
-	if(mysqli_num_rows($result) < 1) {
-		die('{"error": "No results."}');
-	}
-
-	while($row = $result->fetch_assoc()){
-		$rows['info'][] = $row;
-	}
-
-	$conn->close();
-
-	print json_encode($rows);
 ?>
