17cd1df68ed1b4316d0be15094acaa63effff881
lepon01 <leondrolio@gmail.com>
2017-05-18 22:44:12 +0100

    Add random footers

---
+++ js/random.js
@@ -0,0 +1,8 @@
+const random_html = document.getElementById("random");
+const messages = [
+	"I don't even know why I implemented random footers, I have nothing to put here.",
+	"Some say moustacheminer.com was named after a famous block based building game 'Fortresscraft', but this is simply not true.",
+	"Too many subdomains makes your teeth go grey!"
+];
+
+random_html.innerHTML = messages[Math.floor(Math.random() * messages.length)]
