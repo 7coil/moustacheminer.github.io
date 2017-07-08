ad515d43f463fff2189ac835ab3f0f0a5911475e
lepon01 <leondrolio@gmail.com>
2017-07-08 19:35:41 +0100

    change with acronyms and things that make MSS

--- js/random.js
+++ js/random.js
@@ -1,15 +1,26 @@
 const random_html = document.getElementById('random');
 const messages = [
-	'I don\'t even know why I implemented random footers, I have nothing to put here.',
-	'Some say moustacheminer.com was named after a famous block based building game \'Fortresscraft\', but this is simply not true.',
-	'Too many subdomains makes your teeth go grey!',
-	'Days since last weeb invasion: 0 days',
-	'I\'m literally broke for running this website',
-	'What\'s next? An online public Virtual Machine?',
-	'Error 429: You are exceeding b1nzy\'s ratelimit',
-	'The sun is a deadly lazer',
-	'I\'m running out of DDR3 RAM',
-	'Welcome to Moustacheminer Server Services'
+	'Motor Sports Streams',
+	'Maximum Segment Size',
+	'Ministry for State Security',
+	'Merseyside Skeptics Society',
+	'Manufacturers Standardisation Society',
+	'Midland Secondary School',
+	'Master of Strategic Studies',
+	'Microsoft Search Server',
+	'Marshall-Smith syndrome',
+	'Microsatellite stability',
+	'Multispectral Scanner',
+	'Mobile-satellite service',
+	'Managed Security Services',
+	'Mass Storage System',
+	'Microsoft Smooth Streaming',
+	'Miles Sound System',
+	'Modern Shetlandic Scots',
+	'Modular Sleep System',
+	'Managed Security Services',
+	'Management Support System',
+	'Marske Site Services'
 ];
 
 random_html.innerHTML = messages[Math.floor(Math.random() * messages.length)]
